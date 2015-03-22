138deef1477cf6e03e9c4971e762b80e374cb450
Paweł Sułkowski <sulkowski.pawel@gmail.com>
2015-03-22 13:31:22 +0100

    [#2] - Add `App` component that renders hello message

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log('#News App: Smoke test.')
+var React = require('react');
+
+var App = React.createClass({
+  render: function(){
+    return (<h1>Hello, Pilot Academy!</h1>);
+  }
+});
+
+React.render(<App/>, document.body);
