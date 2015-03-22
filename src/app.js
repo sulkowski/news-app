d869f403deba7d4762e6155e26e39b8d041eecb5
Paweł Sułkowski <sulkowski.pawel@gmail.com>
2015-03-22 17:12:11 +0100

    [#3] - Add rendering of stories with mocked stories

--- src/app.js
+++ src/app.js
@@ -1,9 +1,57 @@
 var React = require('react');
 
 var App = React.createClass({
-  render: function(){
-    return (<h1>Hello, Pilot Academy!</h1>);
+  getInitialState: function() {
+    return {
+      stories: [
+        {
+          id: 1,
+          title: "You’re Missing the Point of Server-Side Rendered JavaScript Apps",
+          url: "http://tomdale.net/2015/02/youre-missing-the-point-of-server-side-rendered-javascript-apps/",
+          score: -1,
+          created_at: "2015-02-20T10:12:13.000Z",
+          updated_at: "2015-02-21T00:40:49.339Z"
+        }
+      ]
+    }
+  },
+
+  render: function() {
+    return (
+      <section>
+        <h1>Stories list</h1>
+        <StoriesList stories={this.state.stories}/>
+      </section>
+    );
+  }
+})
+
+var StoriesList = React.createClass({
+  render: function() {
+    var stories = this.props.stories;
+
+    return (
+      <ul>
+        {stories.map(function(story) {
+          return(
+            <StoryItem key={story.id} story={story}/>
+          );
+        })}
+      </ul>
+    );
   }
 });
 
-React.render(<App/>, document.body);
+var StoryItem = React.createClass({
+  render: function() {
+    return(
+      <li>
+        <a href={this.props.story.url}>
+          <span>({this.props.story.score})</span> | {this.props.story.title}
+        </a>
+      </li>
+    );
+  }
+});
+
+React.render(<App />, document.body);
